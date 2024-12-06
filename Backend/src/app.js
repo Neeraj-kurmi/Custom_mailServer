310916e9e346e4f1d6980d05b367d8e2f6ef0fe0
Neeraj-kurmi <neerajkurmi2002@gmail.com>
2024-12-06 14:25:13 +0530

    all set

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const path= require("path")
 
 const authRoutes = require('./routes/authRoutes');
 const emailRoutes = require('./routes/emailRoutes');
@@ -10,10 +11,18 @@ const emailRoutes = require('./routes/emailRoutes');
 const app = express();
 connectDB();
 
+const dirname=path.resolve()
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/email', emailRoutes);
+
+app.use(express.static(path.join(dirname,"/Frontend/dist")))
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(dirname,"Frontend","dist","index.html"))
+})
+
 
 module.exports = app;
